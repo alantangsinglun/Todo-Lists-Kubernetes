d3de71b9e190a8cfa0bd83e0b1c6c52cc56183b6
tangsinglun <cokelight@gmail.com>
2025-04-15 18:46:40 +0800

    first commit

---
+++ k8s-restapi-todo/src/server.ts
@@ -0,0 +1,21 @@
+const express = require('express')
+import bodyParser from 'body-parser';
+import { IndexRouter } from './controllers/v0/index.router';
+const app = express()
+const port = 8080
+
+app.use(bodyParser.json());
+
+//CORS Should be restricted
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET,OPTIONS,POST,DELETE,PATCH");
+    next();
+  });
+
+app.use('/api/v0/', IndexRouter)
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
